5f6e2b4dcd41e1d728786cbe1490f35828f9befd
Rafael Fagundes <rafaelcflima@gmail.com>
2020-08-07 03:51:14 -0300

    Removed unused code and added Material UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,24 @@
-import { GetStaticProps } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import Date from "../components/Date";
-import Layout, { siteTitle } from "../components/Layout";
-import { getSortedPostsData } from "../lib/posts";
-import utilStyles from "../styles/utils.module.css";
-export default function Home({
-  allPostsData,
-}: {
-  allPostsData: {
-    date: string;
-    title: string;
-    id: string;
-  }[];
-}) {
+import styled from "styled-components";
+
+const StyledHome = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #ffff00;
+`;
+
+function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-          voluptas totam voluptatibus iure blanditiis temporibus modi.
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <StyledHome>
+      <Title>Cirkle</Title>
+    </StyledHome>
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-};
+export default Home;
