48b58c4d8f4225399b9694e67919f058317f2294
Rafael Fagundes <rafaelcflima@gmail.com>
2020-08-21 02:54:01 -0300

    Firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,14 @@ import "firebase/auth";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyB6soooBfHdrcL6q1Fy82Cbk5Zur-pWPw0",
+    authDomain: "cirklebr.firebaseapp.com",
+    databaseURL: "https://cirklebr.firebaseio.com",
+    projectId: "cirklebr",
+    storageBucket: "cirklebr.appspot.com",
+    messagingSenderId: "597056782213",
+    appId: "1:597056782213:web:948ff6297ea4642d458eec",
+    measurementId: "G-SGN34W90KP",
   });
 }
 
