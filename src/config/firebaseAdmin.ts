c9ee8719df0bdc3a8a240704741cfe0137c3e32b
Rafael Fagundes <rafaelcflima@gmail.com>
2020-09-11 23:28:11 -0300

    Firebase config changes

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -4,17 +4,17 @@ if (!firebaseAdmin.apps.length) {
   // set variables in next.config.js or on enviroment variables
   // when app is in production mode
   const firebaseServiceAccountKey = {
-    type: process.env.FIREBASE_ADMIN_TYPE,
-    project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    type: "service_account",
+    project_id: "cirklebr",
+    private_key_id: "6af214a0c4f8955cf9dddf909dae1cfe82e0fcc1",
     private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
-    token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+    client_email: "firebase-adminsdk-o1rpm@cirklebr.iam.gserviceaccount.com",
+    client_id: "110635396892820934900",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-o1rpm%40cirklebr.iam.gserviceaccount.com",
   };
 
   firebaseAdmin.initializeApp({
