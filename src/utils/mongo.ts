a86e40973d49c8348a6fb85082fd9505669335a6
Rafael Fagundes <rafaelcflima@gmail.com>
2020-09-12 00:46:02 -0300

    Unused console.log removal

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -1,8 +1,6 @@
 import { Db, MongoClient } from "mongodb";
 
 export async function connectToDatabase(uri: string): Promise<Db> {
-  console.log("uri", uri);
-
   const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
