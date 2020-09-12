f29d4fc61442439e998b93b1ea995a7448407e36
Rafael Fagundes <rafaelcflima@gmail.com>
2020-09-11 22:38:39 -0300

    API changes

--- pages/api/default.ts
+++ pages/api/default.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (request: NowRequest, response: NowResponse): NowResponse => {
-  return response.json({ message: "OK" });
+  return response.json({ endpoint: process.env.API_ENDPOINT });
 };
