72a7ef5ddf7a6e8226b26d79bddd46dde86fabeb
Rafael Fagundes <rafaelcflima@gmail.com>
2020-09-11 22:28:59 -0300

    API endpoint update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    API_ENDPOINT: "https://cirkle-json-server.herokuapp.com",
+    API_ENDPOINT: "http://localhost:3000/api",
     MONGO_DB_URI:
       "mongodb+srv://cirkle:we25rCDopAuuvYTz@cirkle.ppvkf.gcp.mongodb.net/cirkle?retryWrites=true&w=majority",
   },
--- src/modules/menu/MenuService.ts
+++ src/modules/menu/MenuService.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
-import { isDev } from "../../utils/env";
 
 class MenuService {
   async getMenu(): Promise<any> {
-    const res = await axios.get(
-      isDev() ? "http://localhost:3000/api/menu" : "/api/menu"
-    );
+    const res = await axios.get(process.env.API_ENDPOINT + "/menu");
     const data = await res.data;
     return data;
   }
