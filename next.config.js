275f1b8a8c3c923dbb0d554e35d919c9aeed2d01
Rafael Fagundes <rafaelcflima@gmail.com>
2020-09-11 22:50:02 -0300

    Removed static config

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    API_ENDPOINT: "http://localhost:3000/api",
-    MONGO_DB_URI:
-      "mongodb+srv://cirkle:we25rCDopAuuvYTz@cirkle.ppvkf.gcp.mongodb.net/cirkle?retryWrites=true&w=majority",
-  },
-};
