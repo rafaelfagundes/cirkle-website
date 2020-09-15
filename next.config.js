8ae708096c25349dc59a342fa04e781659ad69e0
Rafael Fagundes <rafaelcflima@gmail.com>
2020-09-14 21:59:29 -0300

    Redeploy

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const withPWA = require("next-pwa");
+const withPWA = require("next-pwa");
 
-module.exports = {
+const DEV = {
   publicRuntimeConfig: {
     API_ENDPOINT: process.env.API_ENDPOINT,
   },
 };
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//   },
-//   publicRuntimeConfig: {
-//     API_ENDPOINT: process.env.API_ENDPOINT,
-//   },
-// });
+const PROD = withPWA({
+  pwa: {
+    dest: "public",
+  },
+  publicRuntimeConfig: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
+});
+
+module.exports = process.env.NODE_ENV === "development" ? DEV : PROD;
