6dc230c0037d59731d6ec830acb156e3ba342bfc
Rafael Fagundes <rafaelcflima@gmail.com>
2020-10-21 23:45:54 -0300

    Deploy

--- src/components/MetaData/index.tsx
+++ src/components/MetaData/index.tsx
@@ -22,7 +22,7 @@ function MetaData(props: MetaDataProps): JSX.Element {
       <meta property="quote" content={props.quote} />
       <meta name="description" content={props.description} />
       <meta property="image" content={props.image} />
-      <meta property="og:locale" content="en_US" />
+      <meta property="og:locale" content="pt_BR" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title} />
       <meta property="og:quote" content={props.quote} />
@@ -30,8 +30,12 @@ function MetaData(props: MetaDataProps): JSX.Element {
       <meta property="og:image" content={props.image} />
       <meta content="image/*" property="og:image:type" />
       <meta property="og:url" content={props.url} />
-      <meta property="og:site_name" content="CampersTribe" />
+      <meta property="og:site_name" content="Cirkle" />
       <meta property="og:description" content={props.description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={props.title} />
+      <meta name="twitter:description" content={props.description} />
+      <meta name="twitter:image" content={props.image} />
     </Head>
   );
 }
